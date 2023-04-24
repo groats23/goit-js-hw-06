31ef7aa83867275d7de5fb451fb8a69081836fb2
groats23 <121118901+groats23@users.noreply.github.com>
2023-04-24 10:19:11 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,9 @@
-const categoriesTitles = document.querySelectorAll(".item");
-console.log("Number of categories: ", categoriesTitles.length);
+const categoriesTitles = document.querySelectorAll('.item');
+console.log('Number of categories: ', categoriesTitles.length);
 
-categoriesTitles.forEach((item) => {
-  const categoryName = item.querySelectorAll("h2").textContent;
-  const categoryElements = item.querySelectorAll("li").length;
+categoriesTitles.forEach(item => {
+  const categoryName = item.querySelectorAll('h2').textContent;
+  const categoryElements = item.querySelectorAll('li').length;
 
   console.log(`Category: ${categoryName}`);
   console.log(`Elements: ${categoryElements}`);
